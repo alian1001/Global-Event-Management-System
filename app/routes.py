28da5a450a5044bb5c2c7c25c14c151239f4ca29
ShiftyWizard <o.lynch@me.com>
2022-09-28 12:53:13 +0800

    Add checkout-session route

--- app/routes.py
+++ app/routes.py
@@ -176,3 +176,24 @@ def forgetpassword():
                 return render_template('home.html', title='Home')
 
         return render_template('forgetpassword.html')
+
+### Stripe Implementation
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': '{{PRICE_ID}}',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=request.base_url + '/success',
+            cancel_url=request.base_url + '/cancel',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
