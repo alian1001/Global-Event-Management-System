c89f55e6afa882a7c1c926a9828baf282441cd3c
mattdai123 <110367893+mattdai123@users.noreply.github.com>
2022-09-30 03:52:14 +0800

    hide buttom

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.forms import checkinForm, checkinAndPayForm, eventForm
-from flask import Flask, render_template, redirect, url_for, request, session,current_app
+from flask import Flask, render_template, redirect, url_for, request, session,current_app,jsonify
 from werkzeug.urls import url_parse
 from sqlalchemy import func, extract
 from mapper import user_db, base_db
@@ -13,9 +13,7 @@ from sqlalchemy import create_engine
 mail = Mail()
 mail.init_app(app)
 
-
-   
-
+session1 = 0
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
@@ -34,19 +32,33 @@ def send_email(user_email,token, subject='Reset Your Password', template='reset_
 
 @app.route('/', methods=['GET'])
 def index():
+
     return redirect(url_for('home'))
     
 @app.route('/home',methods=['GET'])
 def home():
-    return render_template('home.html', title='Home')
+    if session1==0:
+        session.clear()
+    status = False
+    print(session.get('username'),'username')
+    if session.get('login')=='OK' and  session.get('username'):
+        status = True
+    return render_template('home.html', title='Home', status=status, username = session.get('username'))
 
 @app.route('/currentevent', methods=['GET'])
 def currentevent():
+    if session1==0:
+        session.clear()
+    status = False
+    print(session.get('username'),'username')
+    if session.get('login')=='OK' and  session.get('username'):
+        status = True
+        
     conn = sqlite3.connect('db.sqlite3')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM 'Event'")
 	
-    return render_template('currentevent.html', title='Current Events', events=cursor.fetchall())
+    return render_template('currentevent.html', title='Current Events', status=status,  events=cursor.fetchall(), username = session.get('username'))
 
 @app.route('/users', methods=['GET'])
 def users():
@@ -94,7 +106,7 @@ def create_event():
         start = str(form.event_time_start.data)
         end = str(form.event_time_end.data)
         location = form.event_location.data
-		
+        
         sql = ''' INSERT INTO Event(eventName, eventHost, eventDate, startTime, endTime, eventLocation)
                        VALUES(?,?,?,?,?,?) '''
         conn = sqlite3.connect('db.sqlite3')
@@ -105,13 +117,18 @@ def create_event():
 
     return render_template('event.html', title = 'Create Event', form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    session['login'] = ''
+    
     if request.method == 'GET':
         return render_template('login.html')
     else:
         username = request.form.get('user_name')
         password = request.form.get('password')
+
 
         # sql = "select * from user where username = '%s'" % username
         # result = base_db.query(sql)
@@ -120,12 +137,16 @@ def login():
         print(result[0][1],password)
         if len(result) != 0:
             if str(result[0][1]) == str(password):
+                print(result,'result')
                 session['username'] = result[0][0]
+                session['login']='OK'
                 session.permanent = True
+                global session1
+                session1 = 1
                 if username == 'admin':
                     return redirect(url_for('admin'))
                 else:
-                    return render_template('home.html')
+                    return redirect(url_for('home'))
             else:
                 return u'Incorrect password.'
         else:
@@ -173,3 +194,12 @@ def forgetpassword():
                 return render_template('home.html', title='Home')
 
         return render_template('forgetpassword.html')
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    """logout"""
+    # clear session
+    session.clear()
+    status = False
+    return  render_template('home.html', title='Home', status=status)
+
