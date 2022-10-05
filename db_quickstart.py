fe9997aa7916db5c75920dfdbc2e524eee912a2c
ShiftyWizard <o.lynch@me.com>
2022-10-06 03:10:22 +0800

    Add default events to quickstart

--- db_quickstart.py
+++ db_quickstart.py
@@ -11,7 +11,9 @@ CREATE TABLE Guest (guestID TEXT PRIMARY KEY NOT NULL, firstName TEXT NOT NULL, 
 CREATE TABLE Dependent (dependentID TEXT PRIMARY KEY NOT NULL, firstName TEXT NOT NULL, lastName TEXT NOT NULL, guestID TEXT REFERENCES Guest (guestID) ON DELETE CASCADE ON UPDATE CASCADE, dietaryReq TEXT, guestRelationship TEXT NOT NULL);
 CREATE TABLE User (userName TEXT NOT NULL UNIQUE PRIMARY KEY, password TEXT NOT NULL, email TEXT, token int);
 
-INSERT INTO User(username,password) VALUES("admin","admin");
+INSERT INTO User(username,password) VALUES("root","root");
+INSERT INTO Event VALUES ("67a459b1","Public Art Exhibit","Jane Artist","2022-12-30","12:00:00","20:00:00","James Oval, UWA","","",0);
+INSERT INTO Event VALUES ("caef73db","Exclusive Music Event","David Musician","2022-12-29","19:00:00","21:00:00","Octagon Theatre, UWA","","prod_MYkBLqlc7Zdv7f",15);
 """
 
 
